d4286fb153e1f3427e558d26cf952d25889f8928
Elena Mikhaylova <elena_tuzkova@yahoo.com>
2019-01-31 19:30:50 +0300

    fix step 2

--- __tests__/test.js
+++ __tests__/test.js
@@ -6,28 +6,37 @@ import httpAdapter from 'axios/lib/adapters/http';
 import { promises as fs } from 'fs';
 import loadPage from '../src';
 
-nock.disableNetConnect();
-
 axios.defaults.adapter = httpAdapter;
 
 const hostname = 'https://hexlet.io';
 const pathname = '/courses';
 const urlSource = `${hostname}${pathname}`;
+const pathFileHtml = path.join(__dirname, '__fixtures__', 'hexlet-io-courses.html');
+const pathFileJs = path.join(__dirname, '__fixtures__/hexlet-io-courses_files/cdn-cgi-scripts-5c5dd728-cloudflare-static-email-decode-min.js');
 
-let tmpDir;
+describe('get html', () => {
+  let tmpDir;
 
-beforeEach(async () => {
-  tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'test-'));
-});
+  beforeAll(() => {
+    nock.disableNetConnect();
 
-test('#get', async () => {
-  const content = await fs.readFile(path.join(__dirname, '__fixtures__', 'hexlet-io-courses.html'), 'utf-8');
+    nock(hostname)
+      .get(pathname)
+      .replyWithFile(200, pathFileHtml);
 
-  nock(hostname)
-    .get(pathname)
-    .reply(200, content);
+    nock(hostname)
+      .get('/cdn-cgi-scripts-5c5dd728-cloudflare-static-email-decode-min.js')
+      .replyWithFile(200, pathFileJs);
+  });
 
-  const loadFileName = await loadPage(urlSource, tmpDir);
-  const expectedHtml = await fs.readFile(loadFileName, 'utf-8');
-  expect(expectedHtml).toEqual(content);
+  beforeEach(async () => {
+    tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), path.sep));
+  });
+
+  it('#links', async () => {
+    const expectedHtml = await fs.readFile(pathFileHtml, 'utf-8');
+    const loadFileName = await loadPage(urlSource, tmpDir);
+    const received = await fs.readFile(loadFileName, 'utf-8');
+    expect(received).toEqual(expectedHtml);
+  });
 });
